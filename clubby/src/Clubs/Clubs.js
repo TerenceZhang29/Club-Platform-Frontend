58ae8196c859f2c2aac897e0bc938a13ffd31a32
Ruiqi Zhu <rz327@cornell.edu>
2020-06-28 22:55:14 -0400

    End of week2

--- clubby/src/Clubs/Clubs.js
+++ clubby/src/Clubs/Clubs.js
@@ -2,66 +2,65 @@ import React, { Component } from 'react';
 import '../css/Clubs.css'
 
 class Clubs extends Component {
-    render() {
-        return (
-          <div class="clublistpage">
+  render() {
+    return (
+      <div class="clublistpage">
+      <body>
+        <h1>Clubs</h1>
+        <div class='filter-list-container'>
+        <Filter />
+        <ClubList />
+        </div>
+      </body>
     
-            <body>
-              <h1>Clubs</h1>
-              <div class='filter-list-container'>
-                <Filter />
-                <ClubList />
-              </div>
-            </body>
-    
-          </div>
-        );
-      }
+      </div>
+    );
+  }
 }
 
 class Filter extends React.Component {
-    render() {
-      return (
-        <div class="filter">
-          <h2>Filter</h2>
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div class="filter">
+        <h2>Filter</h2>
+      </div>
+    );
+  }
+}
+  
+class ClubList extends React.Component {
+  render() {
+    return (
+      <div class="clublist">
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
+      </div>
+    );
+  }
+}
+  
+class ClubButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: null,
+      logo: null,
+      intro: null
+    };
   }
   
-  class ClubList extends React.Component {
-    render() {
-      return (
-        <div class="clublist">
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </div>
-      );
-    }
+  render() {
+    return (
+      <div class='club_bt' >
+        <img src={this.props.logo} />
+        <h3>{this.props.title}</h3>
+        <p>{this.props.intro}</p>
+      </div >
+    );
   }
-  
-  class ClubButton extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        title: null,
-        logo: null,
-        intro: null
-      };
-    }
-  
-    render() {
-      return (
-        <div class='club_bt' >
-          <img src={this.props.logo} />
-          <h3>{this.props.title}</h3>
-          <p>{this.props.intro}</p>
-        </div >
-      );
-    }
-  }
+}
 
 export default Clubs;
--- clubby/src/Events/Events.js
+++ clubby/src/Events/Events.js
@@ -1,33 +1,103 @@
 import React, { Component } from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
 import history from './../history';
+import '../css/Events.css'
 
 class Events extends Component {
-    constructor(props) {
-        super(props);
-        this.cupInfo = this.cupInfo.bind(this);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    cupInfo(event) {
-        history.push('/cupInfo');
-        window.location.reload();
-    }
+  render() {
+    return (
+      // <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
+      //     <div><h2>Event Page</h2>
+      //         <Card style={{ width: '18rem' }}>
+      //             <ListGroup>
+      //                 <ListGroup.Item>AppDev Info Session</ListGroup.Item>
+      //                 <ListGroup.Item onClick={this.cupInfo}>Cup Robotics Info Session</ListGroup.Item>
+      //                 <ListGroup.Item>Drama Society Audition</ListGroup.Item>
+      //             </ListGroup>
+      //         </Card>
+      //     </div>
+      //     <div class='filter-list-container'>
+      //     <EventList />
+      //     <Filter />
+      //     </div>
+      // </div>
+      <div>
+        <h1>Events</h1>
+        <div class='filter-list-container'>
+          <EventList />
+          <Filter />
+        </div>
+      </div>
+    );
+  }
+}
 
-    render() {
-        return (
-            <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-                <div><h2>Event Page</h2>
-                    <Card style={{ width: '18rem' }}>
-                        <ListGroup>
-                            <ListGroup.Item>AppDev Info Session</ListGroup.Item>
-                            <ListGroup.Item onClick={this.cupInfo}>Cup Robotics Info Session</ListGroup.Item>
-                            <ListGroup.Item>Drama Society Audition</ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </div>
-            </div>
-        );
-    }
+class Filter extends React.Component {
+  render() {
+    return (
+      <div class="filter">
+        <h2>Filter</h2>
+      </div>
+    );
+  }
+}
+  
+class EventList extends React.Component {
+  render() {
+    return (
+      <div class="eventlist">
+        <ListGroup>
+        <ListGroup.Item>
+          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+        </ListGroup.Item>
+        </ListGroup>
+      </div>
+    );
+  }
+}
+  
+class EventButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: null,
+      logo: null,
+      intro: null
+    };
+    this.cupInfo = this.cupInfo.bind(this);
+  }
+
+  cupInfo(event) {
+    console.log("Sheldon")
+    history.push('/cupInfo');
+    window.location.reload();
+  }
+  
+  render() {
+    return (
+      <div class='event_item' >
+      <img src={this.props.logo} onClick={this.cupInfo}/>
+      <h3>{this.props.title}</h3>
+      <p>{this.props.intro}</p>
+      </div >
+    );
+  }
 }
 
 export default Events;
