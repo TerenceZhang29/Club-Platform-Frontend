0d87193bc2d4e1a9e06a1e99125805053611d477
Haocheng Shen <hs668@cornell.edu>
2020-06-28 21:04:35 -0400

    add features to clublist

--- clubby/src/Clubs/Clubs.js
+++ clubby/src/Clubs/Clubs.js
@@ -2,66 +2,89 @@ import React, { Component } from 'react';
 import '../css/Clubs.css'
 
 class Clubs extends Component {
-    render() {
-        return (
-          <div class="clublistpage">
-    
-            <body>
-              <h1>Clubs</h1>
-              <div class='filter-list-container'>
-                <Filter />
-                <ClubList />
-              </div>
-            </body>
-    
+  render() {
+    return (
+      <div class="clublistpage">
+
+        <body>
+          <h1>Clubs</h1>
+          <div class='filter-list-container'>
+            <Filter />
+            <ClubList />
           </div>
-        );
-      }
+        </body>
+
+      </div>
+    );
+  }
 }
 
 class Filter extends React.Component {
-    render() {
-      return (
-        <div class="filter">
-          <h2>Filter</h2>
+  render() {
+    return (
+      <div class="filter">
+        <h2>Filter</h2>
+      </div>
+    );
+  }
+}
+
+class ClubList extends React.Component {
+  render() {
+    return (
+      <div class="clublist">
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} tags={["engineering", "computer science"]} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} tags={["engineering", "computer science"]} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} tags={["engineering", "computer science"]} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} tags={["engineering", "computer science"]} />
+        <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} tags={["engineering", "computer science"]} />
+      </div>
+    );
+  }
+}
+
+class ClubButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: null,
+      logo: null,
+      intro: null,
+      tags: null,
+    };
+  }
+
+  renderClubTags() {
+
+    var tags = []
+
+    for (var i = 0; i < this.props.tags.length; i++) {
+      tags.push(<div class='club_tag'>{this.props.tags[i]}</div>)
+    }
+
+    return (
+      <div class='tag_wrapper'>
+        {tags}
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div class='club_bt' >
+        <div><img src={this.props.logo} /></div>
+
+        <div class='clubinfo'>
+          <div class='title'>{this.props.title}</div>
+          <div>{this.renderClubTags()}</div>
+          <div>{this.props.intro}</div>
         </div>
-      );
-    }
+
+        <div class="sub_bt">Subscribe</div>
+
+      </div >
+    );
   }
-  
-  class ClubList extends React.Component {
-    render() {
-      return (
-        <div class="clublist">
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-          <ClubButton title={"Cornell Appdev"} logo={require('./App+Dev+Logo+-+Red.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </div>
-      );
-    }
-  }
-  
-  class ClubButton extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        title: null,
-        logo: null,
-        intro: null
-      };
-    }
-  
-    render() {
-      return (
-        <div class='club_bt' >
-          <img src={this.props.logo} />
-          <h3>{this.props.title}</h3>
-          <p>{this.props.intro}</p>
-        </div >
-      );
-    }
-  }
+}
 
 export default Clubs;
