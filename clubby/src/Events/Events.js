a9373dec134cf7c48eb8258460cc89f78300e611
Ruiqi Zhu <rz327@cornell.edu>
2020-07-12 00:02:37 -0400

    Display event list from backend

--- clubby/src/Events/Events.js
+++ clubby/src/Events/Events.js
@@ -8,48 +8,55 @@ class Events extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: null
+      title: null,
+      events: []
     };
-    this.run_blockly = this.run_blockly.bind(this);
+    // this.events = [];
+    this.getAllEvents = this.getAllEvents.bind(this);
+    this.extractData = this.extractData.bind(this);
+    this.getAllEvents();
   }
 
-  run_blockly(event) {
-    console.log("Chuck Norris");
+  extractData(response) {
+    var responseData = response.data['data'];
+    for (var i = 0; i < responseData.length; i++) {
+      var localEvents = this.state.events;
+      localEvents.push({title: responseData[i]['name']});
+      this.setState({events: localEvents})
+      console.log("IN EXTRACTDATA:")
+      console.log(this.state.events.length)
+    }
+  }
+
+  getAllEvents() {
     axios({
       method: 'GET',
-      url: 'http://localhost:5000/api/events',
+      url: 'http://127.0.0.1:5000/api/events/',
     })
-      .then(function (response) {
-        console.log(response.data);
-      })
+      .then((response) => this.extractData(response))
       .catch(function (error) {
-        console.warn("NO");
-      });
-
+        console.log(error)
+      })
   }
 
   render() {
+    // var events = [
+    //   {title: 'Cornell Cup Robotics Info Session'},
+    //   {title: 'Cornell Chinese Drama Society Audition'},
+    //   {title: 'Cornell AppDev Info Session'}
+    // ];
+    // while (this.events.length == 1) {
+
+    // }
+    console.log("this.events is: ");
+    console.log(this.state.events);
+    var fake = [{title: "WHAT THE FOOK IS GOING ON!"}, {title: "MY MAN WHAT THE HELL"}];
     return (
-      // <div style={{ display: 'flex', justifyContent: 'center', padding: 30 }}>
-      //     <div><h2>Event Page</h2>
-      //         <Card style={{ width: '18rem' }}>
-      //             <ListGroup>
-      //                 <ListGroup.Item>AppDev Info Session</ListGroup.Item>
-      //                 <ListGroup.Item onClick={this.cupInfo}>Cup Robotics Info Session</ListGroup.Item>
-      //                 <ListGroup.Item>Drama Society Audition</ListGroup.Item>
-      //             </ListGroup>
-      //         </Card>
-      //     </div>
-      //     <div class='filter-list-container'>
-      //     <EventList />
-      //     <Filter />
-      //     </div>
-      // </div>
       <div>
-        <button onClick={this.run_blockly}>run blockly</button>
+        {/* <button onClick={this.run_blockly}>run blockly</button> */}
         <h1>Events</h1>
         <div class='filter-list-container'>
-          <EventList />
+          <EventList events={this.state.events}/>
           <Filter />
         </div>
       </div>
@@ -69,27 +76,34 @@ class Filter extends React.Component {
   
 class EventList extends React.Component {
   render() {
-    return (
-      <div class="eventlist">
-        <ListGroup>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        </ListGroup>
-      </div>
-    );
+    var eventComponents = this.props.events.map(function(event) {
+      return <div> 
+               <button>{event.title}</button>
+             </div>;
+    });
+    return <div>{eventComponents}</div>;
+
+    // return (
+    //   <div class="eventlist">
+    //     <ListGroup>
+    //     <ListGroup.Item>
+    //       <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+    //     </ListGroup.Item>
+    //     <ListGroup.Item>
+    //       <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+    //     </ListGroup.Item>
+    //     <ListGroup.Item>
+    //       <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+    //     </ListGroup.Item>
+    //     <ListGroup.Item>
+    //       <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+    //     </ListGroup.Item>
+    //     <ListGroup.Item>
+    //       <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
+    //     </ListGroup.Item>
+    //     </ListGroup>
+    //   </div>
+    // );
   }
 }
   
