c23bc401084ac7247fea1692f89fa3736722a599
Elaine Yu <sy372@cornell.edu>
2020-07-29 16:20:57 +0800

    added hover dropdown menu bar

--- clubby/src/components/Navbar.js
+++ clubby/src/components/Navbar.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../css/Navbar.css';
 import { Navbar, Nav, Form, Button, FormControl, NavDropdow, Dropdown } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
 
 const Navigation = (props) => {
     console.log(props);
@@ -10,34 +9,45 @@ const Navigation = (props) => {
         <Navbar bg="primary" variant="light" expand="lg">
             <Navbar.Brand class="logo" href="/"><img class="logo" src={require("../Clubby_Logo_cropped.JPG")} /></Navbar.Brand>
             <ul class="nav-container">
-                <li><Nav.Link id="home-page" href="/">&nbsp;&nbsp;Home&nbsp;&nbsp;</Nav.Link></li>
-                <li><Nav.Link id="clubs-page" href="/Clubs">&nbsp;&nbsp;Clubs&nbsp;&nbsp;</Nav.Link></li>
-                <li><Nav.Link id="events-page" href="/Events">&nbsp;&nbsp;Events&nbsp;&nbsp;</Nav.Link></li>
-                <li><Nav.Link id="applications-page" href="/Applications">Applications</Nav.Link></li>
+                <li class="nav-tags" ><Nav.Link id="home-page" href="/">&nbsp;&nbsp;Home&nbsp;&nbsp;</Nav.Link></li>
+                <li class="nav-tags" ><Nav.Link href="/Clubs">&nbsp;&nbsp;Clubs&nbsp;&nbsp;</Nav.Link></li>
+                <li class="nav-tags" ><Nav.Link href="/Events">&nbsp;&nbsp;Events&nbsp;&nbsp;</Nav.Link></li>
+                <li class="nav-tags" ><Nav.Link href="/Applications">Applications</Nav.Link></li>
                 <li id="search-area"><input type="text" class="search-input" placeholder="  Search" /></li>
-                <li id="dropdown-area"><img id="dropdown-icon" src={require("../menu-bar.png")} />
+                <li className="dropdown" id="dropdown-area">
+                    <img className="menu" id="dropdown-icon" src={require("../menu-bar.png")} />
+                    <div id="dropdown-container">
+                        <ul id="dropdown-details">
+                            <li class="dropdown-options"><Nav.Link href="/Clubs">Profile</Nav.Link></li>
+                            <li class="dropdown-options"><Nav.Link href="/Clubs">Setting</Nav.Link></li>
+                            <li class="dropdown-options"><Nav.Link href="/Clubs">Help</Nav.Link></li>
+                            <li class="dropdown-options"><Nav.Link href="/Clubs">Log out</Nav.Link></li>
+                        </ul></div>
                 </li>
             </ul>
         </Navbar>
     )
+}
 
-// class Navigation extends React.Component {
-//     render() {
-//         return (
+window.onload = function () {
+    var menu = document.getElementsByClassName("dropdown")[0];
 
-//             <Navbar bg="primary" variant="dark">
-//                 <Navbar.Collapse id="basic-navbar-nav">
-//                     <Nav className="mr-auto">
-//                         <Nav.Link href="/">Home</Nav.Link>
-//                         <Nav.Link href="/Clubs">Clubs</Nav.Link>
-//                         <Nav.Link href="/Events">Events</Nav.Link>
-//                         <Nav.Link href="/Applications">Applications</Nav.Link>
-//                     </Nav>
-//                 </Navbar.Collapse>
-//             </Navbar>
-//         )
-//     }
-// }
+    console.log(menu);
+    menu.addEventListener("mouseover", function () {
+        var tar = this.getElementsByTagName("div")[0]
+        // console.log(tar);
+        // console.log(event.target);
+        tar.style.display = "block";
+    }, false);
+
+
+    menu.addEventListener("mouseout", function () {
+        var tar = this.getElementsByTagName("div")[0]
+        // console.log(tar);
+        // console.log(event.target);
+        tar.style.display = "none";
+    }, false);
+}
 
 // const Navigation = (props) => {
 //     console.log(props);
