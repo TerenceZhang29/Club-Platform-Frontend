19fe2b65c7e7a9fb361a9b32d23b8ea6aa033909
terence-zhang-829 <terencezhang829@gmail.com>
2020-08-02 11:56:59 -0400

    partly finished events page

--- clubby/src/Events/Events.js
+++ clubby/src/Events/Events.js
@@ -37,41 +37,79 @@ class Events extends Component {
 }
 
 class Filter extends React.Component {
+  renderClickableBox(text) {
+    return (
+      <div class="filter-check-box">
+        <p><a href=""></a>{text}</p>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div class="filter">
         <h2>Filter</h2>
+        <div class='favorite-events-filter'>
+          {this.renderClickableBox('Favorite Events')}
+        </div>
+        <div class='search-bar-filter'>
+          <p>Search</p>
+          <textarea></textarea>
+        </div>
+        <div class='category-search-filter'>
+          <p>Category</p>
+          <textarea></textarea>
+          <div class='category-list'>
+            {this.renderClickableBox("Engineering")}
+            {this.renderClickableBox("Business")}
+            {this.renderClickableBox("Sport")}
+            {this.renderClickableBox("Pre-law")}
+            {this.renderClickableBox("Art")}
+          </div>
+        </div>
+        <div class='registered-filter'>
+          <p>Registered</p>
+          <div class='registered-list'>
+            {this.renderClickableBox("10 - 20")}
+            {this.renderClickableBox("20 - 50")}
+            {this.renderClickableBox("50 - 100")}
+            {this.renderClickableBox("100 - 200")}
+          </div>
+        </div>
       </div>
     );
   }
 }
-  
+
 class EventList extends React.Component {
   render() {
     return (
-      <div class="eventlist">
-        <ListGroup>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <EventButton title={"Cornell Appdev"} logo={require('./hack.png')} intro={"Cornell Univeristy's open source app development project team"} />
-        </ListGroup.Item>
-        </ListGroup>
+      <div class='event-container'>
+        <div class='event-item'>
+          <div class='event-item-1'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+          <div class='event-item-2'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+          <div class='event-item-3'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+          <div class='event-item-4'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+          <div class='event-item-5'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+          <div class='event-item-6'>
+            <EventButton title={"BigRedHacks"} logo={require('./hack.png')} intro={"BigRedHacks is the oldest student-run, large-scale hackathon at Cornell University."} />
+          </div>
+        </div>
       </div>
     );
   }
 }
-  
+
 class EventButton extends React.Component {
   constructor(props) {
     super(props)
@@ -88,14 +126,94 @@ class EventButton extends React.Component {
     history.push('/cupInfo');
     window.location.reload();
   }
-  
+
   render() {
     return (
       <div class='event_item' >
-      <img src={this.props.logo} onClick={this.cupInfo}/>
-      <h3>{this.props.title}</h3>
-      <p>{this.props.intro}</p>
+        <img src={this.props.logo} onClick={this.cupInfo} />
+        <h3>{this.props.title}</h3>
+        <p>{this.props.intro}</p>
+        {/* <button type='buttom' onClick={style.color = "#776DC8;"}>Register</button> */}
+        <div class='register-buttom'>
+          <RegisterButtom title={"Register"} count={0} bgColor={'#BCB5F7'} onClick={this.handleclick}></RegisterButtom>
+        </div>
+        <div class='heart-buttom'>
+          <HeartButtom logo={require('./Heart.png')} clicked={0} onClick={this.handleclick}></HeartButtom>
+        </div>
       </div >
+    );
+  }
+}
+
+class RegisterButtom extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Register",
+      count: 0,
+      bgColor: '#BCB5F7'
+    };
+    this.handleclick = this.handleclick.bind(this);
+  }
+
+  handleclick() {
+    if (this.state.count) {
+      this.setState(this.state = ({ title: "Register", count: 0, bgColor: '#BCB5F7' }));
+    }
+    else {
+      this.setState(this.state = ({ title: "Registered", count: 1, bgColor: '#776DC8' }));
+    }
+  }
+
+  render() {
+    return (
+      // **** clickable button with changing text and color ****
+      <button onClick={this.handleclick} style={{ backgroundColor: this.state.bgColor, borderRadius: 10, border: "hidden" }}>
+        {this.state.title}
+      </button>
+
+      // **** changing color only ****
+      // <div className="regi-buttom"
+      //   style={{ backgroundColor: this.state.bgColor }}
+      //   onClick={this.handleclick}
+      // >{this.props.title}
+      // </div>
+    );
+  }
+}
+
+class HeartButtom extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      logo: null,
+      clicked: null
+    };
+    // this.handleclick = this.handleclick.bind(this);
+  }
+
+  state = {
+    open: true
+  }
+  toggleImage = () => {
+    this.setState(state => ({ open: !state.open }))
+  }
+  getImageName = () => this.state.open ? 'filled_heart' : 'Heart'
+
+  // handleclick() {
+  //   if (this.state.clicked) {
+  //     this.setState(this.state = ({ logo: require('./Heart.png'), clicked: 0 }));
+  //   }
+  //   else {
+  //     this.setState(this.state = ({ logo: require('./filled_heart.png'), clicked: 1 }));
+  //   }
+  // }
+
+  render() {
+    const imageName = this.getImageName();
+    return (
+      // <img src={this.props.logo} onClick={this.handleclick} />
+      <img src={require('./' + imageName + '.png')} onClick={this.toggleImage} />
     );
   }
 }
